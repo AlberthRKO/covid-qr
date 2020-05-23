76782040f558f9b16d05ce203bb17b7567a26ed2
AlberthRKO <orlando.alb77@gmail.com>
2020-05-23 12:38:02 -0400

    Arreglos detallados del diseño

--- js/main.js
+++ js/main.js
@@ -72,8 +72,8 @@
 	    var href = $.attr(this, 'href');
 
 	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
-	    }, 500, function() {
+	        scrollTop: $($.attr(this, 'href')).offset().top - 50
+	    }, 600, function() {
 	    	// window.location.hash = href;
 	    });
 		});
